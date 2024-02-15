1b718726471a93d023a758ed852f0c8d6bb74e17
SimranKamboj320 <simrankamboj320@gmail.com>
2024-02-15 16:10:28 +0530

    Initial commit

---
+++ JavaExpert.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class JavaExpert{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+
+        for(int line=1; line<=4; line++){
+            char ch='A';
+            for(int i=1; i<=line; i++){
+                System.out.print(ch++);
+        }
+        System.out.println();
+    }
+}
+}
+
