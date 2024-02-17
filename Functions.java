762803cc37c909934260930297c961ed3183022f
SimranKamboj320 <simrankamboj320@gmail.com>
2024-02-17 13:33:16 +0530

    Functions.java
    I create a functions name as HelloWorld.

---
+++ Functions.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class JavaExpert{
+    public static void printHelloWorld(){
+        System.out.println("Hello World");
+    }
+
+    public static void main(String args[]){
+        printHelloWorld();
+}
+}
