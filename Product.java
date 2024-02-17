08948ec98d002f33244cd64175d33e905eb1dcdc
SimranKamboj320 <simrankamboj320@gmail.com>
2024-02-17 23:37:26 +0530

    Product of a & b
    Product of two numbers using call by value.

---
+++ Product.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class JavaExpert{
+    public static int multiply(int a, int b){
+        int product = a*b;
+        return product;
+    }
+
+    public static void main(String args[]){
+        int a = 3;
+        int b = 5;
+        int prod = multiply(a,b);
+        System.out.println("a*b=" + prod);
+}
+}
