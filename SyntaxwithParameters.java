a6e637a41479b472485a8843a40cebed71c47123
SimranKamboj320 <simrankamboj320@gmail.com>
2024-02-17 13:48:14 +0530

    Syntax with parameters
    I work with parameters using syntax concept.

---
+++ SyntaxwithParameters.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class JavaExpert{
+    public static int calculateSum( int num1, int num2 ){
+        int sum = num1 + num2;
+        return sum;
+    }
+    public static void main(String args[]){
+        Scanner sc=new Scanner (System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int sum = calculateSum(a,b);
+        System.out.println("Sum is: "+ sum);
+}
+}
