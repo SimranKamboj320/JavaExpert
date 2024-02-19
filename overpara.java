184b5313001ebb33c1209fbd31ea828d1f11434e
SimranKamboj320 <simrankamboj320@gmail.com>
2024-02-20 00:23:04 +0530

    Overloading of functions using parameter.
    Overloading of functions using parameter sum function.

---
+++ overpara.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class JavaExpert{
+    public static int sum(int a , int b){
+        return a+b;
+    }
+
+    public static int sum(int a , int b, int c){
+        return a+b+c;
+    }
+
+    public static void main(String args[]){
+        System.out.println(sum(2, 5));
+        System.out.println(sum(2, 5, 1));
+}
+}
