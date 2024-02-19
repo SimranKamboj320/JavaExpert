d088ff8e3970d36e8a46d918aeab8e2221932a36
SimranKamboj320 <simrankamboj320@gmail.com>
2024-02-20 00:40:24 +0530

    Function overloading using datatypes.
    Function overloading using datatypes.

---
+++ overdata.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class JavaExpert{
+    public static int sum(int a , int b){
+        return a+b;
+    }
+
+    public static float sum(float a , float b){
+        return a+b;
+    }
+
+    public static void main(String args[]){
+        System.out.println(sum(2, 5));
+        System.out.println(sum(2.6f, 5.0f));
+}
+}
