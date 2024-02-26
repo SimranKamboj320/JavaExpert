e09b24b56b72dbacc81fee052383c951aeea311c
SimranKamboj320 <simrankamboj320@gmail.com>
2024-02-26 10:49:20 +0530

    Binary to decimal number.
    Binary to decimal number using methods.

---
+++ BintoDec.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class JavaExpert{
+    public static void BintoDec(int binNum){
+        int pow=0;
+        int decNum=0;
+        
+        while(binNum>0){
+            int lastDigit=binNum%10;
+            decNum = decNum+ (lastDigit * (int)Math.pow(2, pow));
+
+            pow++;
+            binNum=binNum/10;
+        }
+        System.out.println("decimal of "+binNum+"="+decNum);
+        
+    }
+
+    public static void main(String args[]){
+        BintoDec(101);
+        
+    }
+}
