9d0c523ffecc97f7d46f684fe9c51e425e0c9900
SimranKamboj320 <simrankamboj320@gmail.com>
2024-02-26 23:15:11 +0530

    Finding prime number
    Finding prime number in some number range.

--- PrimeRange.java
+++ PrimeRange.java
@@ -1,17 +1,28 @@
 import java.util.*;
 
-public class JavaExpert{
-    public static boolean isEven(int n){
-        if(n%2=0){
+public class JavaExpert {
+    public static boolean isPrime(int n) {
+        if (n == 2) {
             return true;
         }
-        else
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
                 return false;
             }
         }
-        
+        return true;
     }
-    public static void main(String args[]){
-        isEven(60 );
+
+    public static void primeInRange(int n) {
+        for (int i = 2; i <= n; i++) {
+            if (isPrime(i)) {
+                System.out.print(i + " ");
+            }
+        }
+        System.out.println(); // Added to print a newline after printing primes in the range
+    }
+
+    public static void main(String args[]) {
+        primeInRange(60);
     }
 }
