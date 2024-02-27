cc140b8fa2f9fc9e4d484c9201791685e3367b24
SimranKamboj320 <simrankamboj320@gmail.com>
2024-02-27 23:31:59 +0530

    Sum of integers using function
    Sum of integers using function

---
+++ IntSum.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class JavaExpert {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Input an integer: ");
+        int digits = sc.nextInt();
+        System.out.println("The sum is " + sumDigits(digits));
+    }
+
+    public static int sumDigits(int n) {
+        int sumOfDigits = 0;
+        while (n > 0) {
+            int lastDigit = n % 10;
+            sumOfDigits += lastDigit;
+            n /= 10;
+        }
+        return sumOfDigits;
+    }
+}
+
---
+++ Sum.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class JavaExpert{
+    public static int sum(int a, int b){
+        int sum = a+b;
+        return sum;
+    }
+
+    public static void main(String args[]){
+        int a = 3;
+        int b = 5;
+        int sum = sum(a,b);
+        System.out.println("a+b=" + sum);
+}
+}
