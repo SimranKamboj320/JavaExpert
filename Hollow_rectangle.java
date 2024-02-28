ee5cb50ec166b335c014c97fe8649263c9ae2d26
SimranKamboj320 <simrankamboj320@gmail.com>
2024-02-28 10:26:08 +0530

    Hollow Rectangle
    Hollow rectangle implementation.

---
+++ Hollow_rectangle.java
@@ -0,0 +1,21 @@
+//import java.util.*;
+
+public class JavaExpert{
+    public static void hollow_rectangle(int totRows, int totcols){
+        //Outer Loop
+        for(int i=1; i<=totRows; i++){
+            for(int j=1; j<=totRows; j++){
+                if(i==1||i==totRows||j==1||j==totcols){
+                    System.out.print("*");
+                }else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String args[]){
+        hollow_rectangle(4,5);
+}
+}
