2d95d8fed71587481fe2725f49f90550829c6b96
SimranKamboj320 <simrankamboj320@gmail.com>
2024-04-02 00:59:40 +0530

    0 1 triangle pattern

---
+++ zero_one_triangle.java
@@ -0,0 +1,18 @@
+public class JavaExpert{
+    public static void zero_one_triangle(int n){
+        //Outer Loop
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=i; j++){
+                if( (i+j) % 2==0 ){
+                    System.out.print("1");
+                } else {
+                    System.out.print("0");
+                }
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String args[]){
+        zero_one_triangle(5);
+}
+}
